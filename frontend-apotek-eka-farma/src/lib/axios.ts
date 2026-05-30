a9685cfec68c5982c8dbb86e782b2b3e41a3f35e
Afdha Auliya Atiq <afdhaauliyaatiq68@gmail.com>
2026-05-30 22:44:23 +0700

    feat: make API_BASE_URL configurable via environment variables

--- frontend-apotek-eka-farma/src/lib/axios.ts
+++ frontend-apotek-eka-farma/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-export const API_BASE_URL = 'http://localhost:5001'
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001'
 
 const api = axios.create({
     baseURL: `${API_BASE_URL}/api`,
